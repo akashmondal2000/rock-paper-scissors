9c1995235af391d8e95158db928bd5a189a55363
akashmondal2000 <mondalakash494@gmail.com>
2024-04-14 18:32:49 +0530

    Update the game

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const startGameBtn = document.getElementById('start-game-btn'); 
+const startGameBtn = document.getElementById("start-game-btn");
 
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
 
-const getPlayerChoice = function() {
-  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const seclection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS} ?` , "").toUpperCase();
+
   if (
-    selection !== ROCK &&
-    selection !== PAPER &&
-    selection !== SCISSORS
+    seclection !== ROCK &&
+    seclection !== PAPER &&
+    seclection !== SCISSORS
   ) {
-    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    alert(`Invalid Choice, We chose ${DEFAULT_USER_CHOICE} for you `);
     return DEFAULT_USER_CHOICE;
   }
-  return selection;
+  return seclection;
 };
 
-startGameBtn.addEventListener('click', function() {
-  console.log('Game is starting...');
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+startGameBtn.addEventListener("click", function () {
+  if(gameIsRunning){
+    return;
+  }
+  gameIsRunning = true
+  console.log("Game is starting...");
+  const playerChoice =  getPlayerChoice();
+  console.log(playerChoice);
 });
